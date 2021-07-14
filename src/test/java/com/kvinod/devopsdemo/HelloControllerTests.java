21610b326a0e89f48cf0fd3849b5149e87a659dd
Vinod Kumar Kayartaya <kayartaya.vinod@gmail.com>
2021-07-14 23:05:05 +0530

    add mvc tests

---
+++ src/test/java/com/kvinod/devopsdemo/HelloControllerTests.java
@@ -0,0 +1,42 @@
+package com.kvinod.devopsdemo;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class HelloControllerTests {
+
+    @Autowired
+    HelloController controller;
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void shouldGetHelloController() {
+        assertNotNull(controller);
+    }
+
+    @Test
+    void shouldGetDefaultHelloMessage() throws Exception {
+        mockMvc.perform(get("/api/hello")).andExpect(status().isOk())
+                .andExpect(content().string(containsString("Hello, friend!")));
+    }
+
+    @Test
+    void shouldGetCustomHelloMessage() throws Exception {
+        mockMvc.perform(get("/api/hello?name=Vinod")).andExpect(status().isOk())
+                .andExpect(content().string(containsString("Hello, Vinod!")));
+    }
+
+}
